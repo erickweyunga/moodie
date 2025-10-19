9b6d4abf76eecda07376cb87cc20b87315284d5d
erickweyunga <aftertechnologies@hotmail.com>
2025-10-19 20:45:37 +0300

    convo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Page() {
+"use client";
+
+import { useRouter } from "next/navigation";
+import { createChat } from "@/utils/chat-store";
+import { Button } from "@/components/ui/button";
+
+export default function Convo() {
+  const router = useRouter();
+
+  const handleNewChat = async () => {
+    try {
+      const chatId = await createChat();
+
+      router.push(`/convo/${chatId}`);
+    } catch (err) {
+      console.error("Failed to create chat:", err);
+    }
+  };
+
   return (
-    <div>
-      <h1>landing page</h1>
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">Conversation</h1>
+        <Button onClick={handleNewChat}>Start New Chat</Button>
+      </div>
     </div>
   );
 }
